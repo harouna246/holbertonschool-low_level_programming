a9b2932f1d28c5798f0ce6f7f03e033ab838f0b5
harouna246 <keitaharouna115@gmail.com>
2025-06-16 13:08:42 +0000

    ques.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,7 @@
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
