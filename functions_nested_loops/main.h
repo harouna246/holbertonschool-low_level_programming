5caf2b7417c97a5fa516ff52516b502c316c22c5
Your Name <you@example.com>
2025-06-18 16:05:02 +0200

    question1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,14 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int print_last_digit(int n);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
 void print_alphabet(void);
 
 #endif
