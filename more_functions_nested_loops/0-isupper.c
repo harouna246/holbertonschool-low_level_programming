1be3d74d6bf70f9fd797b960f3a2babde6178c3e
harouna246 <keitaharouna115@gmail.com>
2025-06-24 12:15:38 +0000

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - Vérifie si un caractère est une majuscule
+ * @c: Le caractère à tester
+ *
+ * Return: 1 si majuscule, 0 sinon
+ */
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
