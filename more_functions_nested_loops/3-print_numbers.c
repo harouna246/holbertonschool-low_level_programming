899fe818d7198ac3052b97dc3557159e1d187267
harouna246 <keitaharouna115@gmail.com>
2025-06-24 15:21:54 +0000

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9, followed by a new line.
+ * Only uses _putchar twice.
+ */
+void print_numbers(void)
+{
+    char nums[] = "0123456789\n";
+    int i;
+
+    for (i = 0; nums[i] != '\0'; i++)
+        _putchar(nums[i]);
+}
