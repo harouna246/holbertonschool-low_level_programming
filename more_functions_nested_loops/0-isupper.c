40f969bafd0b344c7976e3179a312b45f87a1ba0
harouna246 <keitaharouna115@gmail.com>
2025-06-24 13:58:07 +0000

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
+#include "main.h"
+
 /**
  * _isupper - Vérifie si un caractère est une majuscule
- * @c: Le caractère à tester
- *
+ * @c: caractère à tester
  * Return: 1 si majuscule, 0 sinon
  */
 int _isupper(int c)
 {
     return (c >= 'A' && c <= 'Z');
 }
+
