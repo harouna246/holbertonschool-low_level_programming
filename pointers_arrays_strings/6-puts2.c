f4e93f317a4a9e982875c411bd4182bc6834c858
harouna246 <keitaharoun115@gmail.com>
2025-06-27 08:44:39 +0000

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-        {
-            _putchar(str[i]);
-        }
-        i++;
-    }
-    _putchar('\n'); /* Inclure ceci seulement si une nouvelle ligne est attendue */
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n'); /* Inclure ceci seulement si une nouvelle ligne est attendue */
 }
