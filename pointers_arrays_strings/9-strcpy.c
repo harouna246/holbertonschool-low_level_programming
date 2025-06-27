c4d3c45321938d5df6a852bd91563f0a5ce0f66a
harouna246 <keitaharoun115@gmail.com>
2025-06-27 15:07:17 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	// Copier chaque caractère, y compris le '\0'
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0'; // N'oublie pas de copier le caractère nul
+	return (dest);
+}
+
