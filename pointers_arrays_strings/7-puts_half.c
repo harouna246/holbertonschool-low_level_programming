2174fc0379c241d43806dd2b4fc0aba139f2e396
harouna246 <keitaharoun115@gmail.com>
2025-06-27 09:32:01 +0000

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,19 @@ void puts_half(char *str)
 {
     int i, len = 0, start;
 
-    // Calculer la longueur de la chaîne
+    /* Calculer la longueur de la chaîne
     while (str[len] != '\0')
     {
         len++;
     }
 
-    // Déterminer le point de départ : moitié ou (longueur + 1)/2 si impaire
+    /* Déterminer le point de départ : moitié ou (longueur + 1)/2 si impaire
     if (len % 2 == 0)
         start = len / 2;
     else
         start = (len + 1) / 2;
 
-    // Afficher à partir de start
+    /* Afficher à partir de start
     for (i = start; i < len; i++)
     {
         _putchar(str[i]);
