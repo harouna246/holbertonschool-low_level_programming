3d55215902f1d8364a9ae0a1005e44c4b08bd481
harouna246 <keitaharouna115@gmail.com>
2025-06-30 13:42:33 +0000

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The integer value, or 0 if no numbers found
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0, found_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
+	}
+	return (sign * result);
+}
