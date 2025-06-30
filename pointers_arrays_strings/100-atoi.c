4999eb08f9f44c0a1fafb9660ae9fc660645d945
harouna246 <keitaharouna115@gmail.com>
2025-06-30 13:44:31 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,32 @@
+#include <limits.h>
+
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to convert
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
  *
- * Return: The integer value, or 0 if no numbers found
+ * Return: The integer value, or 0 if no numbers are found.
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0, found_digit = 0;
+	int i = 0, sign = 1, result = 0;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			found_digit = 1;
-			result = result * 10 + (s[i] - '0');
+			num = s[i] - '0';
+
+			/* Check for overflow before updating result */
+			if (result > (INT_MAX - num) / 10)
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
+			result = result * 10 + num;
 		}
-		else if (found_digit)
+		else if (result != 0)
 			break;
 		i++;
 	}
